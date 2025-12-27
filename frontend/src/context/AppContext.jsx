ec4fd1bbe27837e11115cd8ef57f4e9d29a0224e
Guilherme Souza <guilhermesouza23022006@gmail.com>
2025-12-27 13:15:11 -0300

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://nutrilife-backend-2421.onrender.com"
     console.log('BACKEND URL =>', backendUrl)
 
     const [doctors, setDoctors] = useState([])
